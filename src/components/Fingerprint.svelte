4a1a50b1fd0df2127e501b7ea80a872635185cc4
ACsóka <acsokacsaba@gmail.com>
2023-02-13 04:28:21 +0100

    Added font sizing

--- src/components/Fingerprint.svelte
+++ src/components/Fingerprint.svelte
@@ -3,12 +3,27 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-12 col-md-6" style="background-color: red;">asd</div>
-    <div class="d-none d-md-flex col-6" style="background-color: green;">
+    <div class="col-12 col-md-8" style="background-color: red;">
+      <div class="column">
+        <h1 class="col-12 text-center text-md-left fingerText">
+          Gestisci e proteggi i tuoi documenti confidenziali
+        </h1>
+        <div class="col-12">gombok</div>
+      </div>
+    </div>
+    <div class="d-none d-md-flex col-4" style="background-color: green;">
       asd
     </div>
   </div>
 </div>
 
 <style>
+  .fingerText {
+    font-family: "titillium";
+    font-size: 6.25rem;
+    font-weight: 400;
+    line-height: 6.25rem;
+    font-style: normal;
+    color: #ffffff;
+  }
 </style>
