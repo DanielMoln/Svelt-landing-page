7e35559a0c89caf27479a39b5f1fc12c8db0888e
ACsóka <acsokacsaba@gmail.com>
2023-02-13 01:39:18 +0100

    Finished navbar

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -3,33 +3,95 @@
 </script>
 
 <nav
-  class="nav navbar wrapper d-flex justify-content-between align-items-center"
+  class="navbar navbar-expand-md navbar-dark wrapper d-flex w-100"
+  style="padding-left: 0 !important; padding-right: 0 !important;"
 >
-  <div class="container d-flex justify-content-between align-items-center">
-    <a class="navbar-brand d-flex align-items-center" href="#">
-      <img
-        src={logo}
-        alt="Logo"
-        class="d-inline-block"
-        style="vertical-align: middle; width: 41px; height: 41px"
-      />
-      <span class="d-inline-block align-text-center">DBLOCK</span>
-    </a>
-
-    <ul class="d-flex flex-row align-items-center justify-content-between">
-      <p class="nav-link"><a href="#">HOME</a></p>
-      <p class="nav-link"><a href="#">FEATURES</a></p>
-      <p class="nav-link"><a href="#">CHI SIAMO</a></p>
-    </ul>
-
-    <button class="contactBtn">CONTATTI</button>
+  <div class="col-md-12 background" />
+  <div class="container">
+    <div class="container">
+      <div class="row">
+        <div
+          class="col-12 col-md-4 d-flex justify-content-end justify-content-md-start"
+        >
+          <a
+            class="navbar-brand d-flex align-items-center justify-content-end col-8"
+            href="#"
+          >
+            <img
+              src={logo}
+              alt="Logo"
+              class="d-inline-block"
+              style="vertical-align: middle; width: 41px; height: 41px"
+            />
+            <span class="d-inline-block align-text-center">DBLOCK</span>
+          </a>
+          <div class="d-flex justify-content-end col-4">
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon" />
+            </button>
+          </div>
+        </div>
+        <div
+          class="col-12 col-md-8 row-cols-md collapse navbar-collapse align-items-center d-md-flex "
+          id="navbarNav"
+        >
+          <div
+            class="col-12 col-md-6 d-flex justify-content-center align-items-center align-text-center align-items-center"
+          >
+            <ul
+              class="d-md-flex column row-cols-md justify-content-center align-items-center text-center w-100"
+              style="text-align: center;"
+            >
+              <p class="nav-link"><a href="#">HOME</a></p>
+              <p class="nav-link"><a href="#">FEATURES</a></p>
+              <p class="nav-link"><a href="#">CHI SIAMO</a></p>
+            </ul>
+          </div>
+          <div
+            class="col-12 col-md-6 d-flex align-items-center justify-content-center justify-content-md-end"
+          >
+            <button class="contactBtn">CONTATTI</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </nav>
 
+<!--
+
+
+
+
+
+-->
 <style>
+  .background {
+    position: absolute;
+    background-color: black;
+    opacity: 0.5;
+
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (max-width: 767.98px) {
+    .background {
+      background-color: #00131e;
+      opacity: 1;
+    }
+  }
+
   .wrapper {
     background-image: url("../assets/images/header_bg.png");
-    height: 80px !important;
   }
   ul {
     margin: 0 !important;
@@ -38,14 +100,16 @@
   .nav-link {
     margin: 0 10px !important;
     padding: 0 !important;
-  }
 
-  a {
     font-family: titillium;
     font-weight: bold;
     font-size: 14px;
     color: white !important;
 
     white-space: nowrap;
+  }
+  a {
+    color: #fff !important;
+    font-weight: 900;
   }
 </style>
